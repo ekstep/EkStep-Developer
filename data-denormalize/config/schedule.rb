c9dbb537f29e0af5a09a6f880d73436c45c841f3
Kiran G <kirang20@gmail.com>
2016-07-04 15:01:29 +0530

    ekstep/Ecosystem-Platform/#280 | Move partner denormalize job to Ekstep-developer repo

--- data-denormalize/config/schedule.rb
+++ data-denormalize/config/schedule.rb
@@ -3,5 +3,5 @@ set :environment_variable, 'EP_LOG_DIR'
 set :environment, ENV['EP_LOG_DIR']
 
 # every 1.day, :at => '1:00 am' do
-#   rake "scheduled:ekstep_data_decrypt"
+#   rake "scheduled:ekstep_data_denormalize"
 # end
