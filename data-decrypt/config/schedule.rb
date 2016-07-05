d2464eb6af99b845e27e6c98676137d96f0380a9
Kiran G <kirang20@gmail.com>
2016-07-05 09:49:01 +0530

    ekstep/Ecosystem-Platform/#280 | Schedule for data decrypt and denormalize jobs

--- data-decrypt/config/schedule.rb
+++ data-decrypt/config/schedule.rb
@@ -2,6 +2,6 @@ set :output, {:error => 'error.log', :standard => 'cron.log'}
 set :environment_variable, 'EP_LOG_DIR'
 set :environment, ENV['EP_LOG_DIR']
 
-# every 1.day, :at => '1:00 am' do
-#   rake "scheduled:ekstep_data_decrypt"
-# end
+every 1.day, :at => '1:00 am' do
+  rake "scheduled:ekstep_data_decrypt"
+end
