9d065ad32c60459319bd455cf0142ac2759a6c07
Kiran G <kirang20@gmail.com>
2016-07-05 09:58:47 +0530

    ekstep/Ecosystem-Platform/#280 | Merge data import, decrypt and denormalize into single module
    Different modules were causing issues with whenever gem

--- data-import/config/schedule.rb
+++ data-import/config/schedule.rb
@@ -5,3 +5,11 @@ set :environment, ENV['EP_LOG_DIR']
 every 1.day, :at => '1:00 am' do
   rake "scheduled:ekstep_data_import"
 end
+
+every 1.day, :at => '1:00 am' do
+  rake "scheduled:ekstep_data_decrypt"
+end
+
+every 1.day, :at => '1:00 am' do
+  rake "scheduled:ekstep_data_denormalize"
+end
